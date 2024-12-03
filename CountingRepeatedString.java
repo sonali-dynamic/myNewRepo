b3947ee4c1c042625637ef4d4a93eac78bd35bd8
sonali-dynamic <145424254+sonali-dynamic@users.noreply.github.com>
2024-12-03 22:37:20 +0530

    Update CountingRepeatedString.java
    fixed error

--- CountingRepeatedString.java
+++ CountingRepeatedString.java
@@ -3,12 +3,13 @@ class CountingRepeatedString {
 		String str = "AABBBEEF" 
         int count = 0;		// A2B3E2F1
 		for(int i=0;i<=str.length();i++) {
-			for(int j=i+1;j<=str.length();j++) {
+			for(int j=0;j<=str.length();j++) {
 				if(str.charAt(i) == str.charAt(j)) {
 					count++;
 				}
 			}
 			System.out.print(str.charAt(i)+""+count);
+			count = 0;
 		}
 	}
   
